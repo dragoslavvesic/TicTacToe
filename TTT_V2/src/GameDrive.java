5783d45103c961b59e4d19444db3fad7a4d77f8d
dragoslavvesic <66559509+dragoslavvesic@users.noreply.github.com>
2020-06-07 20:02:53 +0200

    Add files via upload

---
+++ TTT_V2/src/GameDrive.java
@@ -0,0 +1,14 @@
+/**
+ * <p>This class contains the main driver for the project. It calls the 
+ * constructor of the class that delivers the GUI.</p>
+ * 
+ * @author      Dragoslav Vesic
+ * @version     30/12/2017
+ */
+public class GameDrive {
+
+    public static void main(String[] args) {
+        new GameGUI();
+    }
+    
+}
